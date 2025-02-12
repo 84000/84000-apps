3f10795f886202753f980efc6a97a80bc27e16ed
Grant Damron <grant@ilusiv.io>
2025-02-11 18:03:05 -0800

    update tailwind configurations

--- apps/web-main/tailwind.config.js
+++ apps/web-main/tailwind.config.js
@@ -1,8 +1,15 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
 const sharedConfig = require('../../tailwind.base.config.js');
 
 module.exports = {
   ...sharedConfig,
   content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
     ...sharedConfig.content,
     'apps/web-main/**/*.{ts,tsx,html}', // Specific paths for web-main
   ],
--- libs/design-system/.storybook/main.ts
+++ libs/design-system/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -13,28 +13,43 @@ const config: StorybookConfig = {
     options: {},
   },
   webpackFinal: async (config) => {
-    config?.module?.rules?.push({
+    if (!config.module?.rules) {
+      config.module = { rules: [] };
+    }
+
+    if (!config.module.rules) {
+      config.module.rules = [];
+    }
+
+    // Remove any existing CSS rules
+    config.module.rules = config.module.rules.filter(
+      (rule) => rule?.test?.toString() !== '/\\.css$/'
+    );
+
+    config.module.rules.push({
       test: /\.css$/,
       use: [
         'style-loader',
-        'css-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+          },
+        },
         {
           loader: 'postcss-loader',
           options: {
             postcssOptions: {
-              plugins: [require('tailwindcss'), require('autoprefixer')],
+              plugins: ['tailwindcss', 'autoprefixer'],
             },
           },
         },
       ],
-      include: path.resolve(__dirname, '../'),
+      include: [path.resolve(__dirname, '../src')],
     });
+
     return config;
   },
 };
 
 export default config;
-
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/recipes/storybook/custom-builder-configs
