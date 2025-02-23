4651b3f8abf63ed3da90c243a9e12bf8183d9df9
Grant Damron <grant@ilusiv.io>
2025-02-23 16:39:26 +0100

    add background image

--- apps/web-main/src/app/(auth)/login/page.tsx
+++ apps/web-main/src/app/(auth)/login/page.tsx
@@ -7,10 +7,15 @@ import { MainLogo } from '@design-system';
 
 const LoginPage = () => {
   const { loginWithGoogle } = useSession();
+
+  const bgImage =
+    'bg-[url(https://cdn.prod.website-files.com/661fb69072916f350216ad91/66bd96d7b11732fc3ba19751_Donate-p-2000.jpeg)]';
   return (
     <>
-      <div className="relative min-h-screen flex flex-col justify-center bg-cover bg-center bg-primary">
-        <div className="flex h-full justify-center items-center px-4">
+      <div
+        className={`relative min-h-screen flex flex-col justify-center bg-cover bg-center ${bgImage}`}
+      >
+        <div className="flex h-full justify-center items-center sm:pr-[50%] px-4">
           <CardBox className="sm:max-w-sm w-full border-none p-0">
             <div className="py-8 px-6">
               <div className="flex justify-center items-center py-8">
