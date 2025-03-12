3d0646167f5f56d2cb6350490dfb8ae38cfdd6f0
Grant Damron <grant@ilusiv.io>
2025-03-12 15:07:12 -0700

    move all supabase client use to data-access library

--- apps/web-main/src/app/(auth)/auth/callback/route.ts
+++ apps/web-main/src/app/(auth)/auth/callback/route.ts
@@ -1,5 +1,6 @@
-import { createClient } from '../../../../utils/supabase/ssr';
+import { createServerClient } from '@data-access';
 import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
@@ -7,9 +8,27 @@ export async function GET(request: Request) {
   const next = searchParams.get('next') ?? '/';
 
   if (code) {
-    const supabase = createClient();
+    const cookieStore = await cookies();
+    const dataClient = createServerClient({
+      cookies: {
+        getAll: () => {
+          return cookieStore.getAll();
+        },
+        setAll: (cookiesToSet) => {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    });
 
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    const { error } = await dataClient.auth.exchangeCodeForSession(code);
 
     if (error) {
       console.error(`failed to create session ${error}`);
