2af16bc1cc4f04e51275555f76a0d3628691991b
Grant Damron <grant.damron@84000.co>
2025-03-19 09:00:12 -0700

    Components for translation passage blocks (#21)
    ### Summary
    
    Create components, currently wrappers around typography elements, for
    passage blocks so they show up in the design system.

--- libs/design-system/src/lib/Editor/extensions/Translation/Component.tsx
+++ libs/design-system/src/lib/Editor/extensions/Translation/Component.tsx
@@ -1,12 +1,12 @@
 import { NodeViewContent, NodeViewProps, NodeViewWrapper } from '@tiptap/react';
-import { P } from '../../../Typography/Typography';
+import { TranslationPassage } from '../../../Translation/Passage/Passage';
 
 export default (_props: NodeViewProps) => {
   return (
     <NodeViewWrapper className="passage-translation">
-      <P className="pt-4">
+      <TranslationPassage className="pt-4">
         <NodeViewContent className="passage-translation-content" />
-      </P>
+      </TranslationPassage>
     </NodeViewWrapper>
   );
 };
