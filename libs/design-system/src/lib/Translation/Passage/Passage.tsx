0c5b23d0817cb3e4ea5e0836fdb2b1128355883b
Grant Damron <grant@ilusiv.io>
2025-03-19 17:01:28 -0700

    apply basic translation components

--- libs/design-system/src/lib/Translation/Passage/Passage.tsx
+++ libs/design-system/src/lib/Translation/Passage/Passage.tsx
@@ -1,4 +1,25 @@
+import { BodyItemType } from '@data-access';
 import { H3, P } from '../../Typography/Typography';
+import { ElementType } from 'react';
 
+export const AcknowledgmentPassage = P;
+export const IntroductionPassage = P;
+export const SummaryPassage = P;
 export const TranslationPassage = P;
+
+export const AcknowledgmentHeader = H3;
+export const SummaryHeader = H3;
 export const TranslationHeader = H3;
+
+export const passageComponentForType: { [key in BodyItemType]: ElementType } = {
+  acknowledgment: AcknowledgmentPassage,
+  acknowledgmentHeader: AcknowledgmentHeader,
+  'end-note': P,
+  introduction: IntroductionPassage,
+  summary: SummaryPassage,
+  summaryHeader: SummaryHeader,
+  toc: P,
+  translation: TranslationPassage,
+  translationHeader: TranslationHeader,
+  unknown: P,
+};
