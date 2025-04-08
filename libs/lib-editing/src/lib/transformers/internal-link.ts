0b39f7a26b96143e0ef5655a034b3091d5fd9707
Grant Damron <grant.damron@84000.co>
2025-04-08 15:35:58 -0700

    Blockquote and scan fix (#29)
    ### Summary
    
    - Provides and example of transforming a passage into a different block
    as opposed to applying styles. This will grow in complexity and will
    likely need revision.
    - Fixes an issue of the scan function where annotations of zero length,
    which are common, resulted in the text being removed.
    
    ### Linear
    
    
    [dev-80](https://linear.app/84000/issue/DEV-80/apply-annotations-as-block-transformations)

--- libs/lib-editing/src/lib/transformers/internal-link.ts
+++ libs/lib-editing/src/lib/transformers/internal-link.ts
@@ -2,6 +2,7 @@ import { InternalLinkAnnotation } from '@data-access';
 import { Transformer, scan } from './transformer';
 
 export const internalLink: Transformer = ({ block, annotation }) => {
+  // TODO: transform into a custom mark type
   return scan({
     block,
     annotation,
