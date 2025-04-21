e02b26e79c01926b85045789a465f69f60bf2a37
Grant Damron <grant@ilusiv.io>
2025-04-21 14:20:45 -0700

    extract pagination component and add slider

---
+++ libs/design-system/src/lib/Slider/Slider.stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Slider } from './Slider';
+
+const meta: Meta<typeof Slider> = {
+  component: Slider,
+  title: 'Controls/Slider',
+  tags: ['autodocs'],
+  argTypes: {
+    defaultValue: {
+      control: { type: 'multi-select' },
+    },
+    max: {
+      control: { type: 'number' },
+    },
+    min: {
+      control: { type: 'number' },
+    },
+    step: {
+      control: { type: 'number' },
+    },
+  },
+};
+
+type Story = StoryObj<typeof Slider>;
+
+export const Default: Story = {
+  args: {
+    defaultValue: [50],
+    max: 100,
+    min: 0,
+    step: 1,
+  },
+};
+
+export const Range: Story = {
+  args: {
+    defaultValue: [25, 75],
+    max: 100,
+    min: 0,
+    step: 1,
+  },
+};
+
+export const CustomStep: Story = {
+  args: {
+    defaultValue: [50],
+    max: 100,
+    min: 0,
+    step: 10,
+  },
+};
+
+export default meta; 
