94932473d1ce368d33091b09a35c0931c94f72c4
Grant Damron <grant.damron@84000.co>
2025-05-15 14:14:29 -0700

    Show truncated content on hover (#49)
    ### Summary
    
    Adds tooltips that display cell content on hover for cell types that may
    be truncated.
    
    ### Linear
    
    [DEV-128](https://linear.app/84000/issue/DEV-128/cell-hover-text)

---
+++ apps/web-main/src/components/ui/TooltipCell.tsx
@@ -0,0 +1,22 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from '@design-system';
+
+export const TooltipCell = ({
+  className: classNamse,
+  content,
+}: {
+  className: string;
+  content: string;
+}) => {
+  return (
+    <div className={classNamse}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div className="truncate">{content}</div>
+        </TooltipTrigger>
+        <TooltipContent className="max-w-100 text-wrap" align="start">
+          {content}
+        </TooltipContent>
+      </Tooltip>
+    </div>
+  );
+};
