201ded1fa1554d2c0dd514e9d3597c7fe7c04d57
Grant Damron <grant@ilusiv.io>
2025-07-26 18:17:40 -0700

    debounce onChange events from input components

--- libs/design-system/src/lib/Input/Input.tsx
+++ libs/design-system/src/lib/Input/Input.tsx
@@ -1,22 +1,51 @@
-import * as React from 'react';
+'use client';
+
+import {
+  ChangeEvent,
+  ComponentProps,
+  forwardRef,
+  useEffect,
+  useState,
+} from 'react';
 
 import { cn } from '@lib-utils';
+import { useDebounce } from 'use-debounce';
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-          className,
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
-  },
-);
+const Input = forwardRef<
+  HTMLInputElement,
+  ComponentProps<'input'> & { delay?: number }
+>(({ value, className, type, onChange, delay = 250, ...props }, ref) => {
+  const [inputValue, setInputValue] = useState(value || '');
+  const [changeEvent, setChangeEvent] = useState<
+    ChangeEvent<HTMLInputElement> | undefined
+  >();
+  const [debouncedValue] = useDebounce(changeEvent, delay);
+
+  useEffect(() => {
+    if (!debouncedValue) {
+      return;
+    }
+
+    onChange?.(debouncedValue);
+  }, [debouncedValue, onChange]);
+
+  return (
+    <input
+      type={type}
+      value={inputValue}
+      className={cn(
+        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        className,
+      )}
+      onChange={(e) => {
+        setInputValue(e.target.value);
+        setChangeEvent(e);
+      }}
+      ref={ref}
+      {...props}
+    />
+  );
+});
 Input.displayName = 'Input';
 
 export { Input };
