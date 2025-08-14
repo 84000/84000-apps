7e5bf4cda1645667d29128d95a7dbd9731c5505a
Grant Damron <grant.damron@84000.co>
2025-08-14 16:09:19 -0700

    Expanded works table (#86)
    ### Summary
    
    Most sections in the canon tree do not have works expressly associated
    with them, especially of they are not leaf nodes. With this change,
    parent nodes will display all works in child nodes in their works table.
    
    ### Linear
    
    
    [DEV-276](https://linear.app/84000/issue/DEV-276/canon-resource-view-parents)

--- apps/web-main/src/app/(DashboardLayout)/canon/[slug]/page.tsx
+++ apps/web-main/src/app/(DashboardLayout)/canon/[slug]/page.tsx
@@ -1,4 +1,15 @@
+import { createBrowserClient, getCanonSections } from '@data-access';
 import { CanonPage } from '@lib-canon/ssr';
+
+export const revalidate = 3600;
+
+export const generateStaticParams = async () => {
+  const client = createBrowserClient();
+  const sections = await getCanonSections({ client });
+  return sections.map((section) => ({
+    slug: section.uuid,
+  }));
+};
 
 const Page = CanonPage;
 
