25247582f853d6efc6c545f4c827031ab4bb12be
Grant Damron <grant.damron@84000.co>
2025-09-05 09:00:39 -0700

    Add definition to glossary entries table (#92)
    ### Summary
    
    The definition column had been removed previously. Now it is back, along
    with a utility for stripping HTML tags from string.
    
    ### Linear
    
    
    [DEV-302](https://linear.app/84000/issue/DEV-302/add-definition-into-glossary)

--- libs/lib-utils/src/lib/string.ts
+++ libs/lib-utils/src/lib/string.ts
@@ -1,6 +1,12 @@
 export const removeDiacritics = (str: string) =>
   str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 
+export const removeHtmlTags = (str: string) =>
+  str
+    .replace(/<[^>]*>/g, '')
+    .replace(/\s+/g, ' ')
+    .trim();
+
 export const camelCaseToHuman = (str: string) => {
   const result = str.replace(/([A-Z])/g, ' $1').trim();
   return result.charAt(0).toUpperCase() + result.slice(1);
