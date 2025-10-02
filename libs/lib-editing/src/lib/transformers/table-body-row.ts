4e6a692810b91a77a15a921eba19515f41c64a20
Grant Damron <grant@ilusiv.io>
2025-10-02 15:55:37 -0700

    parse table related annotations

--- libs/lib-editing/src/lib/transformers/table-body-row.ts
+++ libs/lib-editing/src/lib/transformers/table-body-row.ts
@@ -1,3 +1,29 @@
-import { pass } from './transformer';
+import { recurse } from './recurse';
+import { splitBlock } from './split-block';
+import type { Transformer } from './transformer';
 
-export const tableBodyRow = pass;
+export const tableBodyRow: Transformer = (ctx) => {
+  recurse({
+    ...ctx,
+    until: ['paragraph'],
+    transform: (ctx) =>
+      splitBlock({
+        ...ctx,
+        transform: ({ block }) => {
+          block.type = 'tableRow';
+          block.attrs = {
+            ...block.attrs,
+          };
+          block.content = [
+            {
+              type: 'paragraph',
+              content: block.content || [],
+              attrs: {
+                ...block.attrs,
+              },
+            },
+          ];
+        },
+      }),
+  });
+};
