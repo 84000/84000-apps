a68a6c5757640177d1e32f9553e5d8cdff925e5f
Grant Damron <grant@ilusiv.io>
2025-10-07 12:43:16 -0700

    only use search params in client components

--- libs/lib-canon/src/lib/page/Page.tsx
+++ libs/lib-canon/src/lib/page/Page.tsx
@@ -6,15 +6,11 @@ import {
   getCanonTree,
   getCanonWorks,
 } from '@data-access';
-import { ArticlePage } from './ArticlePage';
-import { WorksPage } from './WorksPage';
-import { ScrollArea } from '@design-system';
+import { InnerPage } from './InnerPage';
 
 export const CanonPage = async ({
-  searchParams,
   params,
 }: {
-  searchParams: Promise<Record<string, string>>;
   params: Promise<{ slug: string }>;
 }) => {
   const { slug: uuid } = await params;
@@ -27,24 +23,10 @@ export const CanonPage = async ({
 
   const section = await getCanonSection({ client, uuid });
   const works = await getCanonWorks({ client, uuids });
-  const { tab } = await searchParams;
 
   if (!section) {
     return <div className="text-center">Section not found</div>;
   }
 
-  const scrollAreaClass = `h-full ${tab === 'overview' ? 'bg-gray-50' : 'bg-background'}`;
-
-  return (
-    <ScrollArea className={scrollAreaClass}>
-      <div className="flex flex-row justify-center pt-0 pb-8 px-4 w-full">
-        <div className="w-5/6 max-w-[1080px]">
-          {tab === 'overview' && <ArticlePage section={section} />}
-          {tab === 'texts' && (
-            <WorksPage label={section.label} works={works || []} />
-          )}
-        </div>
-      </div>
-    </ScrollArea>
-  );
+  return <InnerPage section={section} works={works || []} />;
 };
