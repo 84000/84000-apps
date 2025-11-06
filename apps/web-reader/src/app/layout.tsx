26ca328209d366a1c58ff40834e2c97706bdc691
Grant Damron <grant@ilusiv.io>
2025-11-06 09:55:09 -0800

    create web-reader app

---
+++ apps/web-reader/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import type { Metadata } from 'next';
+import '@design-system-css';
+
+export const metadata: Metadata = {
+  title: '84000 Translation Reader',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/svg+xml" />
+      </head>
+      <body>
+        <div className="fixed size-full overflow-auto">{children}</div>
+      </body>
+    </html>
+  );
+}
