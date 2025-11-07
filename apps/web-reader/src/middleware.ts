11c29de812c1f434d0ee5a6df9338099009d4435
Grant Damron <grant.damron@84000.co>
2025-11-07 10:11:59 -0800

    redirect for tohoku number paths (#159)
    ### Summary
    
    Using a tohoku number as a route (e.g. `/toh251`) in the public reader
    will now redirect to the correct work uuid with `toh` query paramter.

---
+++ apps/web-reader/src/middleware.ts
@@ -0,0 +1,48 @@
+import { createServerClient } from '@data-access/ssr';
+import { type NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  // match tohoku catalog shortlinks like /toh1234
+  if (pathname.match(/^\/toh\d+/)) {
+    const url = request.nextUrl.clone();
+    const toh = pathname.split('/')[1]?.split('.')[0];
+
+    if (!toh) {
+      return NextResponse.next();
+    }
+
+    const client = await createServerClient();
+    const { data, error } = await client
+      .from('works')
+      .select('uuid')
+      .eq('toh', toh)
+      .single();
+
+    if (error || !data) {
+      return NextResponse.next();
+    }
+
+    if (data?.uuid) {
+      url.pathname = `/${data.uuid}`;
+      url.searchParams.set('toh', toh);
+    }
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+};
