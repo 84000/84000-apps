36941556bf54b70f70ac17614866b38ae68a01cb
Grant Damron <grant@ilusiv.io>
2025-12-03 10:12:00 -0800

    basic body tile handling

--- libs/lib-editing/src/lib/components/editor/extensions/Heading/Heading.ts
+++ libs/lib-editing/src/lib/components/editor/extensions/Heading/Heading.ts
@@ -1,11 +1,14 @@
 import TiptapHeading from '@tiptap/extension-heading';
 import type { Level } from '@tiptap/extension-heading';
 import {
+  BODY_TITLE_STYLE,
   H1_STYLE,
   H2_STYLE,
   H3_STYLE,
   H4_STYLE,
   H5_STYLE,
+  HONORIFIC_TITLE_STYLE,
+  SECTION_TITLE_STYLE,
 } from '@design-system';
 import { createNodeViewDom } from '../../util';
 import { HTMLElementType } from 'react';
@@ -19,14 +22,33 @@ const CLASS_FOR_LEVEL: Record<Level, string> = {
   6: H5_STYLE,
 };
 
+const CLASS_FOR_CLASS: Record<string, string> = {
+  'section-title': SECTION_TITLE_STYLE,
+  'body-title-main': BODY_TITLE_STYLE,
+  'body-title-honorific': HONORIFIC_TITLE_STYLE,
+};
+
 export const Heading = TiptapHeading.extend({
+  addAttributes() {
+    return {
+      ...this.parent?.(),
+      class: {
+        default: null,
+        parseHTML(element) {
+          return element.getAttribute('class');
+        },
+      },
+    };
+  },
+
   addNodeView() {
     return ({ node, editor, extension, getPos, HTMLAttributes }) => {
       const nodeLevel = parseInt(node.attrs.level, 10) as Level;
       const hasLevel = this.options.levels.includes(nodeLevel);
       const level = hasLevel ? nodeLevel : this.options.levels[0];
       const element = `h${level}` as HTMLElementType;
-      const className = CLASS_FOR_LEVEL[nodeLevel];
+      const className =
+        CLASS_FOR_CLASS[node.attrs.class] || CLASS_FOR_LEVEL[nodeLevel];
 
       const { dom } = createNodeViewDom({
         editor,
