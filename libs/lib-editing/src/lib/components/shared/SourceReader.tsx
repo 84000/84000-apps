981978f1f4e0dcfa7a4f538343f5cc4f1a88aa36
Grant Damron <grant.damron@84000.co>
2025-12-11 08:46:03 -0800

    Toc and Search Styles (#202)
    ### Summary
    
    - Update the table of contents to more closely match the designs
    - Revise search widget colors, which were based on a very plain them
    before
    
    ### Linear
    
    - [DEV-432](https://linear.app/84000/issue/DEV-432)
    - [DEV-438](https://linear.app/84000/issue/DEV-438)

--- libs/lib-editing/src/lib/components/shared/SourceReader.tsx
+++ libs/lib-editing/src/lib/components/shared/SourceReader.tsx
@@ -49,11 +49,12 @@ export const SourceReader = () => {
   }, [toh, uuid]);
 
   return (
-    <div className="pt-12 flex flex-col gap-5 mx-auto max-w-5xl 2xl:max-w-380">
+    <div className="pt-12 flex flex-col gap-5 mx-auto max-w-readable 2xl:max-w-380">
       {folios.map((folio, index) => (
         <LabeledElement
           key={index}
           label={`f.${folio.folio}.${folio.side}\nvol.${folio.volume}`}
+          className="mt-0.5"
         >
           <div className="leading-7 font-tibetan text-base 2xl:whitespace-pre-wrap whitespace-normal">
             {folio.content}
--- libs/lib-search/src/lib/ui/SearchResultTab.tsx
+++ libs/lib-search/src/lib/ui/SearchResultTab.tsx
@@ -10,11 +10,13 @@ export const SearchResultTabs = ({ results }: { results: SearchResults }) => {
             <TabsTrigger
               key={tab}
               value={tab}
-              className="h-full border border-border rounded-xl flex-grow data-[state=inactive]:bg-muted data-[state=active]:border-slate-300 data-[state=active]:border-3 border-2"
+              className="h-full border border-border rounded-xl flex-grow data-[state=inactive]:bg-muted data-[state=active]:border-border data-[state=active]:border-3 border-2"
             >
               <div className="flex flex-col gap-1 items-center">
-                <span className="capitalize text-navy font-bold">{tab}</span>
-                <span className="text-ochre font-bold">
+                <span className="capitalize text-secondary font-bold">
+                  {tab}
+                </span>
+                <span className="text-primary font-bold">
                   {results[tab].length} matches
                 </span>
               </div>
