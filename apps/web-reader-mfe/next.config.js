cc324f658f69be3dfaeced06c2fbe62d5f09622a
Grant Damron <grant@ilusiv.io>
2025-12-15 14:50:57 -0800

    keep mfe env var

--- apps/web-reader-mfe/next.config.js
+++ apps/web-reader-mfe/next.config.js
@@ -1,4 +1,5 @@
 import { composePlugins, withNx } from '@nx/next';
+import { withMicrofrontends } from '@vercel/microfrontends/next/config';
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -7,9 +8,12 @@ const nextConfig = {
   nx: {},
 };
 
+const mfe = process.env.MICROFRONTEND ? [withMicrofrontends] : [];
+
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
+  ...mfe,
 ];
 
 export default composePlugins(...plugins)(nextConfig);
