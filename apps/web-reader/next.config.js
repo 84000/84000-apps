d6e8e1ced66a905c3509167d80b4a0e4a387216b
Grant Damron <grant@ilusiv.io>
2025-12-17 12:17:42 -0800

    remove mfe variant of public reader app

--- apps/web-reader/next.config.js
+++ apps/web-reader/next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
   nx: {},
 };
 
-if (process.env.MICROFRONTEND) {
-  nextConfig.assetPrefix = '/reader-static';
-}
-
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
