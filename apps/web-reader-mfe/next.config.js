d1b21ea8a084aed7421df71c1614f8b3d018b8c2
Grant Damron <grant.damron@84000.co>
2025-12-17 12:21:15 -0800

    NextJs multizone (#223)
    ### Summary
    
    Vercel's micro-frontend framework has some nice optimizations, but it is
    _expensive_. We can use the vanilla NextJs mfe feature instead. A
    separate app is still required because routes need a `/reader` prefix,
    but the change here is to define an asset prefix instead of relying on
    Vercel to do it.

--- apps/web-reader-mfe/next.config.js
+++ apps/web-reader-mfe/next.config.js
@@ -1,19 +1,16 @@
 import { composePlugins, withNx } from '@nx/next';
-import { withMicrofrontends } from '@vercel/microfrontends/next/config';
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {},
+  assetPrefix: '/reader-static',
 };
-
-const mfe = process.env.MICROFRONTEND ? [withMicrofrontends] : [];
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
-  ...mfe,
 ];
 
 export default composePlugins(...plugins)(nextConfig);
