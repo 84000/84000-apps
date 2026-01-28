73682e306686242498cb56777ef17de86ffca5d9
Grant Damron <grant@ilusiv.io>
2026-01-28 15:18:17 -0800

    handle spacing and itemStyle values

--- libs/lib-editing/src/lib/components/editor/extensions/List.ts
+++ libs/lib-editing/src/lib/components/editor/extensions/List.ts
@@ -1,7 +1,19 @@
-import { UL_STYLE } from '@design-system';
 import { cn } from '@lib-utils';
 import { BulletList } from '@tiptap/extension-list';
 import { mergeAttributes } from '@tiptap/react';
+
+const ITEM_STYLE_TO_CLASS: { [key: string]: string } = {
+  none: 'list-none',
+  dots: 'list-[disc]',
+  circles: 'list-[circle]',
+  numbers: 'list-decimal',
+  letters: 'list-[upper-latin]',
+};
+
+const LIST_SPACING_TO_CLASS: { [key: string]: string } = {
+  horizontal: 'ml-4',
+  vertical: '',
+};
 
 export const List = BulletList.extend({
   addOptions() {
@@ -10,16 +22,14 @@ export const List = BulletList.extend({
       keepMarks: false,
       keepAttributes: false,
       ...this.parent?.(),
-      HTMLAttributes: {
-        class: cn(UL_STYLE, 'list-none'),
-      },
+      HTMLAttributes: {},
     };
   },
   addAttributes() {
     return {
       ...this.parent?.(),
       spacing: {
-        default: undefined,
+        default: 'horizontal',
         parseHTML: (element) => element.getAttribute('data-spacing'),
         renderHTML(attributes) {
           return mergeAttributes(attributes, {
@@ -36,16 +46,26 @@ export const List = BulletList.extend({
         renderHTML(attributes) {
           return mergeAttributes(attributes, {
             'data-nesting': `${attributes.nesting}`,
+            class: cn(
+              LIST_SPACING_TO_CLASS[attributes.spacing || 'horizontal'] ||
+                'ml-4',
+            ),
           });
         },
       },
       itemStyle: {
-        default: undefined,
+        default: 'none',
         parseHTML: (element) => element.getAttribute('data-item-style'),
         renderHTML(attributes) {
-          return mergeAttributes(attributes, {
-            'data-item-style': attributes.itemStyle,
-          });
+          return {
+            ...mergeAttributes(attributes, {
+              'data-item-style': attributes.itemStyle,
+            }),
+            class: cn(
+              ITEM_STYLE_TO_CLASS[attributes.itemStyle || 'none'] ||
+                'list-none',
+            ),
+          };
         },
       },
     };
