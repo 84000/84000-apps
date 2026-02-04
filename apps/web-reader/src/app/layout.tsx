e30d1591e786988e79166a84f70db98bcd5fbddd
Grant Damron <grant@ilusiv.io>
2026-02-04 15:40:59 -0800

    update font import method to avoid hydration error

--- apps/web-reader/src/app/layout.tsx
+++ apps/web-reader/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import '@design-system-css';
-import { Fonts } from '@design-system';
+import { FONTS } from '@design-system';
 
 export const metadata: Metadata = {
   title: '84000 Translation Reader',
@@ -14,9 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Fonts />
-      </head>
+      <head>{FONTS}</head>
       <body>
         <div className="fixed size-full overflow-auto">{children}</div>
       </body>
