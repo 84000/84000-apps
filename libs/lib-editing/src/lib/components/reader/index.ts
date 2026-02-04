703b8c38faea35892bb8ff3ccb1907a13c6e8815
Grant Damron <grant@ilusiv.io>
2026-02-04 11:56:35 -0800

    fix reader page import

--- libs/lib-editing/src/lib/components/reader/index.ts
+++ libs/lib-editing/src/lib/components/reader/index.ts
@@ -1,5 +1,4 @@
 export * from './ReaderBackMatterPage';
-// ReaderBodyPage is exported via ssr.ts due to server-only imports
 export * from './ReaderLeftPanelPage';
 export * from './ReaderLayout';
 export * from './TranslationReader';
