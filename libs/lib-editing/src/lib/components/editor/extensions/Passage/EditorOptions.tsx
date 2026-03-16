308a596ca2cf4c4a8a335d926e87a10fa171b646
Grant Damron <grant@ilusiv.io>
2026-03-16 15:46:25 -0700

    first pass of improved endnote management in the editor

--- libs/lib-editing/src/lib/components/editor/extensions/Passage/EditorOptions.tsx
+++ libs/lib-editing/src/lib/components/editor/extensions/Passage/EditorOptions.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { DropdownMenuItem, DropdownMenuSeparator } from '@design-system';
-import { BracesIcon, PencilIcon } from 'lucide-react';
+import { BracesIcon, PencilIcon, Trash2Icon } from 'lucide-react';
 
 export const EditorOptions = ({
   onSelection,
+  type,
 }: {
   onSelection: (item: string) => void;
+  type?: string;
 }) => {
   return (
     <>
@@ -17,6 +19,17 @@ export const EditorOptions = ({
       <DropdownMenuItem onSelect={() => onSelection('attributes')}>
         <BracesIcon className="text-ochre" /> View Attributes
       </DropdownMenuItem>
+      {type === 'endnotes' && (
+        <>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onSelect={() => onSelection('deleteEndnote')}
+            className="text-destructive"
+          >
+            <Trash2Icon className="text-destructive" /> Delete Endnote
+          </DropdownMenuItem>
+        </>
+      )}
     </>
   );
 };
