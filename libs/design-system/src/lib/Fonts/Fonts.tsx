e43cde97e112ed29b392eda878907e4f760fc043
Grant Damron <grant@ilusiv.io>
2026-04-01 14:03:14 -0700

    fix font import values

--- libs/design-system/src/lib/Fonts/Fonts.tsx
+++ libs/design-system/src/lib/Fonts/Fonts.tsx
@@ -12,7 +12,7 @@ export const FONTS = [
   />,
   <link
     key="fonts-stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wdth,wght,YTLC@0,6..12,75..125,200..1000,440..540;1,6..12,75..125,200..1000,440..540&family=Noto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap"
+    href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wdth,wght,YTLC@0,6..12,75..125,200..1000,440..540;1,6..12,75..125,200..1000,440..540&family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap"
     rel="stylesheet"
   />,
 ] as const;
