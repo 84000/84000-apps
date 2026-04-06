334283c15067967b0a2fc06a35910a4a4e96a3b4
Grant Damron <grant@ilusiv.io>
2026-04-06 14:19:47 -0700

    remove side column padding

--- libs/lib-editing/src/lib/components/shared/LeftPanel.tsx
+++ libs/lib-editing/src/lib/components/shared/LeftPanel.tsx
@@ -47,8 +47,8 @@ export const LeftPanel = ({
           <TabsTrigger value="toc">Table of Contents</TabsTrigger>
         </TabsList>
       </div>
-      <div className="px-2 flex-1 min-h-0 mb-2">
-        <div className="overflow-auto h-full rounded bg-surface">
+      <div className="flex-1 min-h-0">
+        <div className="overflow-auto h-full bg-surface">
           <div className="rounded px-2 pb-8 max-w-readable mx-auto">
             <TabsContent value="toc" className="px-2 mt-1.5">
               <TableOfContents
