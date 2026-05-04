4d922139da19ee75d1e7a7c5487e3915137135c4
Grant Damron <grant@ilusiv.io>
2026-05-04 10:02:06 -0700

    add proxy for posthog events

--- libs/lib-instr/src/lib/browser-client.ts
+++ libs/lib-instr/src/lib/browser-client.ts
@@ -14,7 +14,8 @@ export const initializeInstrumentation = () => {
 
   if (POSTHOG_KEY && POSTHOG_HOST) {
     posthog.init(POSTHOG_KEY, {
-      api_host: POSTHOG_HOST,
+      api_host: '/ingest',
+      ui_host: POSTHOG_HOST,
       defaults: '2025-05-24',
       person_profiles: 'identified_only',
     });
