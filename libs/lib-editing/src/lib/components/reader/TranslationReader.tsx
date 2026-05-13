986eab4fcd2404a08727faae99199a8c591c6dd8
Grant Damron <grant@ilusiv.io>
2026-05-13 14:45:56 -0700

    use ssr content in reader

--- libs/lib-editing/src/lib/components/reader/TranslationReader.tsx
+++ libs/lib-editing/src/lib/components/reader/TranslationReader.tsx
@@ -1,8 +1,33 @@
 'use client';
 
+import { cn } from '@eightyfourthousand/lib-utils';
 import { TranslationEditor } from '../editor';
-import { PaginationProvider } from '../editor/PaginationProvider';
+import { PaginationProvider, usePagination } from '../editor/PaginationProvider';
 import { TranslationRenderer, useNavigation } from '../shared';
+import { TranslationSSRContent } from './TranslationSSRContent';
+import type { TranslationEditorContent } from '../editor';
+
+const ReaderBody = ({
+  content,
+  className,
+}: {
+  content: TranslationEditorContent;
+  className?: string;
+}) => {
+  const { editor } = usePagination();
+
+  if (!editor) {
+    return (
+      <div className={cn('flex h-full', className)}>
+        <div className="relative flex flex-col flex-1 h-full">
+          <TranslationSSRContent content={content} className="flex-1" />
+        </div>
+      </div>
+    );
+  }
+
+  return <TranslationEditor className={className} />;
+};
 
 export const TranslationReader = ({
   content,
@@ -22,7 +47,7 @@ export const TranslationReader = ({
       content={content}
       isEditable={false}
     >
-      <TranslationEditor className={className} />
+      <ReaderBody content={content} className={className} />
     </PaginationProvider>
   );
 };
